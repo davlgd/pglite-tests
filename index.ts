c51a1d229b42c5c03762a9bbcf37a5cf97fef6c2
David Legrand <1110600+davlgd@users.noreply.github.com>
2024-02-29 16:35:39 +0100

    show

--- index.ts
+++ index.ts
@@ -27,16 +27,8 @@ async function main() {
         console.log("DB filled with data");
     }
 
-    db.query(`SELECT AVG(duree) AS duree_moyenne FROM films;`).then(console.log);
-            db.query(`
-                SELECT f.titre, r.prenom || ' ' || r.nom AS realisateur
-                FROM films f
-                JOIN films_realisateurs fr ON f.film_id = fr.film_id
-                JOIN realisateurs r ON r.realisateur_id = fr.realisateur_id;
-            `).then((result) => {
-                console.log(result);
-                db.close();
-            }).catch(console.error);
+    await showDB(db);
+    db.close();
 /*
     db.query(`SELECT
     (SELECT count(*) FROM films) AS films,
@@ -56,62 +48,7 @@ async function main() {
                 db.close();
             }).catch(() => {
             console.log("DB is empty, creating tables and inserting data...");
-
-            db.query(`
-CREATE TABLE films (
-    film_id SERIAL PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL,
-    description TEXT,
-    annee_sortie INTEGER,
-    duree INTEGER, -- Durée du film en minutes
-    langue VARCHAR(50)
-);
-`).then(() => {
-                db.query(`
-    INSERT INTO films (titre, description, annee_sortie, duree, langue) VALUES
-    ('Pulp Fiction', 'Les vies de deux hommes de main de la mafia, d''un boxeur, d''un gangster et de sa femme, et de deux braqueurs se tissent dans quatre histoires de violence et de rédemption.', 1994, 154, 'Anglais'),
-    ('Le Voyage de Chihiro', 'Dans le Japon moderne, Chihiro, une fille de dix ans, se retrouve plongée dans un monde fantastique après que ses parents aient été transformés en cochons par une sorcière maléfique.', 2001, 125, 'Japonais');
-    `).then(console.log).catch(console.error);
-            }
-            ).catch(console.error);
-
-            db.query(`
-CREATE TABLE realisateurs (
-    realisateur_id SERIAL PRIMARY KEY,
-    prenom VARCHAR(255) NOT NULL,
-    nom VARCHAR(255) NOT NULL,
-    pays VARCHAR(100),
-    date_naissance DATE
-);
-`).then(() => {
-                db.query(`
-INSERT INTO realisateurs (prenom, nom, pays, date_naissance) VALUES
-('Quentin', 'Tarantino', 'USA', '1963-03-27'),
-('Hayao', 'Miyazaki', 'Japon', '1941-01-05');
-`).then(console.log).catch(console.error);
-            }).catch(console.error);
-        }
-    }
-    ).catch(console.error);
-
-            db.query(`
-CREATE TABLE films_realisateurs (
-    film_id INTEGER NOT NULL,
-    realisateur_id INTEGER NOT NULL,
-    FOREIGN KEY (film_id) REFERENCES films (film_id),
-    FOREIGN KEY (realisateur_id) REFERENCES realisateurs (realisateur_id),
-    PRIMARY KEY (film_id, realisateur_id)
-);
-`).then(() => {
-                db.query(`
-    INSERT INTO films_realisateurs (film_id, realisateur_id) VALUES
-    ((SELECT film_id FROM films WHERE titre = 'Pulp Fiction'), (SELECT realisateur_id FROM realisateurs WHERE nom = 'Tarantino')),
-    ((SELECT film_id FROM films WHERE titre = 'Le Voyage de Chihiro'), (SELECT realisateur_id FROM realisateurs WHERE nom = 'Miyazaki'));
-    `).then(console.log).catch(console.error);
-            }).catch(console.error);
-        }
-    }
-    ).catch(console.error);*/
+*/
 }
 
 async function checkDB(db: PGlite): Promise<boolean> {
@@ -167,4 +104,17 @@ async function fillinDB(db: PGlite): Promise<void> {
         ((SELECT film_id FROM films WHERE titre = 'Le Voyage de Chihiro'), (SELECT realisateur_id FROM realisateurs WHERE nom = 'Miyazaki'));
     `);
 }
+
+async function showDB(db: PGlite) {
+    const average = await db.query(`SELECT AVG(duree) AS duree_moyenne FROM films;`);
+    console.log(average);
+    const films = await db.query(`
+        SELECT f.titre, r.prenom || ' ' || r.nom AS realisateur
+        FROM films f
+        JOIN films_realisateurs fr ON f.film_id = fr.film_id
+        JOIN realisateurs r ON r.realisateur_id = fr.realisateur_id;
+    `)
+    console.log(films);
+}
+
 main().catch(console.error);
